d04ebd9af16255458ba741258dec908f5767c92d
Tales Garcia <garcia.tales@gmail.com>
2020-11-04 22:07:42 -0300

    delete unused code

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -3,11 +3,6 @@ import AppointmentDao from '../daos/AppointmentController';
 
 const routes = Router();
 
-interface AppointmentInterface {
-  provider: string,
-  date: Date
-}
-
 routes.post('/', AppointmentDao.create)
 
 export default routes;
