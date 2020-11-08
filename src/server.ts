1ca5f09959c494154c6974f9cdd4348db8997781
Tales Garcia <garcia.tales@gmail.com>
2020-11-08 13:31:38 -0300

    added static folder route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes/index.routes';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
+app.use('/images', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
