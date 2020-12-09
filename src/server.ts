3a707e89d6eb60cbd5432d615d6eabe4f845b135
Tales Garcia <garcia.tales@gmail.com>
2020-12-09 12:20:08 -0300

    feat(cors): enable cors in api

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes/index.routes';
 import path from 'path';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/images', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
