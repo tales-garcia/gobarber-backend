413d5c31d3848f22de3423fbd7e4735bf21e023c
Tales Garcia <garcia.tales@gmail.com>
2020-12-22 17:32:20 -0300

    ci(js output): put javascript output inside a tmp folder

--- src/shared/infra/http/routes/appointments.routes.ts
+++ src/shared/infra/http/routes/appointments.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import AppointmentControlller from '../../../../modules/appointment/controllers/AppointmentController';
+import AppointmentControlller from '@modules/appointment/controllers/AppointmentController';
 import authentication from './middlewares/authentication';
 
 const routes = Router();
