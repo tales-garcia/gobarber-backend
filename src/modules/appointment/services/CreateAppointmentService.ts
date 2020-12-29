c38cdb065ac7f1bee1a8f9b3802c53509a2d47c3
Tales Garcia <garcia.tales@gmail.com>
2020-12-29 17:14:57 -0300

    feat(services): create services
    Create appointment module services

---
+++ src/modules/appointment/services/CreateAppointmentService.ts
@@ -0,0 +1,34 @@
+import IAppointmentDAO from "@modules/appointment/DAOs/IAppointmentDAO";
+import AppError from "@shared/errors/AppError";
+import { parseISO, startOfHour } from "date-fns";
+import { inject, injectable } from "tsyringe";
+
+interface Request {
+  date: string;
+  providerId: string;
+}
+
+@injectable()
+export default class CreateAppointmentService {
+  constructor(
+    @inject('AppointmentDAO')
+    private appointmentDao: IAppointmentDAO
+  ) { }
+
+  public async execute({ providerId, date }: Request) {
+    const parsedDate = startOfHour(parseISO(date));
+
+    const appointmentInSameDate = await this.appointmentDao.findByDate(parsedDate);
+
+    if (appointmentInSameDate) {
+      throw new AppError('Error: Appointment already booked', 400);
+    }
+
+    const appointment = await this.appointmentDao.create({
+      providerId,
+      date: parsedDate
+    });
+
+    return appointment;
+  }
+}
