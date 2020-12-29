6332a49c80acacd4b8ce90b556da652c0b6fef9b
Tales Garcia <garcia.tales@gmail.com>
2020-12-29 17:15:22 -0300

    feat(services): apply services to appointment modules

--- src/modules/appointment/infra/http/appointments.routes.ts
+++ src/modules/appointment/infra/http/appointments.routes.ts
@@ -1,15 +1,12 @@
 import { Router } from 'express';
 import AppointmentController from './controllers/AppointmentController';
 import authentication from '@modules/user/infra/http/middlewares/authentication';
-import { container } from 'tsyringe';
 
 const routes = Router();
 
 routes.use(authentication);
 
-const appointmentController = container.resolve(AppointmentController);
-
-routes.post('/', appointmentController.create.bind(appointmentController));
-routes.get('/', appointmentController.index.bind(appointmentController));
+routes.post('/', AppointmentController.create);
+routes.get('/', AppointmentController.index);
 
 export default routes;
--- src/modules/appointment/infra/http/controllers/AppointmentController.ts
+++ src/modules/appointment/infra/http/controllers/AppointmentController.ts
@@ -1,45 +1,36 @@
 import { Request, Response } from "express";
-import { startOfHour, parseISO } from 'date-fns';
-import IAppointmentDAO from "../../../DAOs/IAppointmentDAO";
-import { inject, injectable } from 'tsyringe';
+import AppointmentDAO from "../../mongoose/DAOs/AppointmentDAO";
+import CreateAppointmentService from "@modules/appointment/services/CreateAppointmentService";
+import { container } from "tsyringe";
+import AppError from "@shared/errors/AppError";
 
-@injectable()
-export default class AppointmentsController {
-  constructor(
-    @inject('AppointmentDAO')
-    private appointmentDAO: IAppointmentDAO
-  ) {}
-
+export default {
   async create(req: Request, res: Response) {
     const { providerId, date } = req.body;
 
-    const parsedDate = startOfHour(parseISO(date));
-
-    const appointmentInSameDate = await this.appointmentDAO.findByDate(parsedDate);
-
-    if(appointmentInSameDate) {
-      return res.status(400).json({ msg: 'Error: Appointment already booked' });
-    }
-
     try {
-      const appointment = await this.appointmentDAO.create({
+      const appointment = await container.resolve(CreateAppointmentService).execute({
         providerId,
-        date: parsedDate
+        date
       });
 
       return res.status(201).json(appointment);
-    } catch(e) {
+    } catch (e) {
+      if(e instanceof AppError) {
+        return res.status(e.statusCode).json({ msg: e.message });
+      }
+
       console.log(e);
       return res.status(500).json({ msg: 'Error: Failed at creating appointment' });
     }
-  }
+  },
   async index(req: Request, res: Response) {
     try {
-      const appointments = await this.appointmentDAO.find();
+      const appointments = await new AppointmentDAO().find();
 
       return res.status(200).json(appointments);
 
-    } catch(e) {
+    } catch (e) {
       console.log(e);
       return res.status(500).json({ msg: 'Error: Failed at listing appointments' })
     }
