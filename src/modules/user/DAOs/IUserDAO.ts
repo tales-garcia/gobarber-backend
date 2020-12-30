f68bcf8f9d6b331e48a0f3c355d7ac6be78ebcfb
Tales Garcia <garcia.tales@gmail.com>
2020-12-30 18:03:37 -0300

    fix(dao interface): interface should not depend on mongoose

--- src/modules/user/DAOs/IUserDAO.ts
+++ src/modules/user/DAOs/IUserDAO.ts
@@ -1,9 +1,8 @@
-import { MongooseUpdateQuery } from "mongoose";
 import IUserDTO from "../DTOs/IUserDTO";
 
 export default interface IUserDAO {
   findByEmail(email: string): Promise<IUserDTO>;
-  findByIdAndUpdate(id: string, query: MongooseUpdateQuery<IUserDTO>): Promise<IUserDTO>;
+  findByIdAndUpdate(id: string, query: IUserDTO): Promise<IUserDTO>;
   create(user: object): Promise<Omit<IUserDTO, "password">>;
   findById(id: string): Promise<IUserDTO>;
   find(filter?: object): Promise<Omit<IUserDTO, "password">[]>;
