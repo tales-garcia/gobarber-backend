cee619a52b3809adbb9ada8a0c85d12f3f9b7fc5
Tales Garcia <garcia.tales@gmail.com>
2020-12-31 17:37:47 -0300

    test(authenticate user service): create authenticateUserService test

---
+++ src/modules/user/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,45 @@
+import 'reflect-metadata';
+import UserDAOMock from "../infra/mongoose/DAOs/mocks/UserDAOMock";
+import CreateUserService from "./CreateUserService";
+import AuthenticateUserService from './AuthenticateUserService';
+import AppError from '@shared/errors/AppError';
+
+describe('Create user', () => {
+  it('should be able to authenticate a created user', async () => {
+    const userDao = new UserDAOMock();
+
+    await new CreateUserService(userDao).execute(
+      {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+        avatar: '3'
+      }
+    );
+
+    const response = await new AuthenticateUserService(userDao).execute({
+      email: 'johndoe@example.com',
+      password: '123456'
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response).toHaveProperty('user');
+  });
+  it('should not be able to authenticate a created user with a wrong password', async () => {
+    const userDao = new UserDAOMock();
+
+    await new CreateUserService(userDao).execute(
+      {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+        avatar: '3'
+      }
+    );
+
+    expect(new AuthenticateUserService(userDao).execute({
+      email: 'johndoe@example.com',
+      password: '1234567'
+    })).rejects.toBeInstanceOf(AppError);
+  });
+});
