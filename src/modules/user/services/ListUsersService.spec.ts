45f82eaabddeb0a26579184a86b046179a92d705
Tales Garcia <garcia.tales@gmail.com>
2020-12-31 17:37:11 -0300

    test(list users service): create list user service test

---
+++ src/modules/user/services/ListUsersService.spec.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/errors/AppError';
+import 'reflect-metadata';
+import UserDAOMock from "../infra/mongoose/DAOs/mocks/UserDAOMock";
+import CreateUserService from "./CreateUserService";
+import ListUsersService from './ListUsersService';
+
+describe('Create user', () => {
+  it('should be able to list users', async () => {
+    const userDao = new UserDAOMock();
+
+    const users = await new ListUsersService(userDao).execute();
+
+    expect(users).toStrictEqual([]);
+  });
+  it('should be able to list a created user', async () => {
+    const userDao = new UserDAOMock();
+
+    await new CreateUserService(userDao).execute(
+      {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+        avatar: '3'
+      }
+    );
+
+    const users = await new ListUsersService(userDao).execute();
+
+    expect(users).toHaveLength(1);
+  });
+});
