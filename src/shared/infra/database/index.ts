b392500016cd3f75bbe385851563a65fad2fd354
Tales Garcia <garcia.tales@gmail.com>
2021-01-03 17:17:47 -0300

    fix(mongoose connect): promise definition was throwing an error

--- src/shared/infra/database/index.ts
+++ src/shared/infra/database/index.ts
@@ -1,6 +1,6 @@
 import mongoose, { ConnectionOptions } from 'mongoose';
 
 mongoose.connect(`mongodb://${process.env.NODE_ENV === 'prod' ? 'mongo' : 'localhost'}:27017/gobarber`, { useMongoClient: true } as ConnectionOptions);
-mongoose.Promise = global.Promise;
+require('mongoose').Promise = global.Promise;
 
 export default mongoose;
