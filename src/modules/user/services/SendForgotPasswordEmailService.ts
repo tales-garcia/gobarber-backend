eb184315abe3deda91c9f0438ea630281d7115b1
Tales Garcia <garcia.tales@gmail.com>
2021-01-05 18:20:47 -0300

    feat(services): create send forgot password service
    Create simple service which verifies the user and sends a simple email

---
+++ src/modules/user/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,23 @@
+import IMailProvider from "@shared/container/providers/MailProvider/models/IMailProvider";
+import AppError from "@shared/errors/AppError";
+import { inject, injectable } from "tsyringe";
+import IUserDAO from "../DAOs/IUserDAO";
+
+@injectable()
+export default class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UserDAO')
+    private userDao: IUserDAO,
+    private mailProvider: IMailProvider
+  ) { }
+
+  public async execute({ email }: Assign<object, "email", string>) {
+    const user = await this.userDao.findByEmail(email);
+
+    if(!user) {
+      throw new AppError('Error: User not found', 404);
+    }
+
+    this.mailProvider.sendMail(email, 'Recuperação de senha');
+  }
+}
