ce65472c58afc3dec23166030aa1b1284664a6db
Tales Garcia <garcia.tales@gmail.com>
2021-01-07 18:18:41 -0300

    feat(usertokendao mock): implement findByToken method
    findByToken uses findOne method

--- src/modules/user/DAOs/mocks/UserTokenDAOMock.ts
+++ src/modules/user/DAOs/mocks/UserTokenDAOMock.ts
@@ -2,7 +2,7 @@ import IUserTokenDTO from "@modules/user/DTOs/IUserTokenDTO";
 import { uuid } from "uuidv4";
 import IUserTokenDAO from "../IUserTokenDAO";
 
-type IUserToken = Assign<IUserTokenDTO, "_id" & "token", string>;
+type IUserToken = Assign<IUserTokenDTO, "_id" | "token", string>;
 
 export default class UserTokenDAOMock implements IUserTokenDAO {
   private tokens: IUserToken[] = [];
@@ -17,5 +17,17 @@ export default class UserTokenDAOMock implements IUserTokenDAO {
     this.tokens.push(token);
 
     return token;
+  }
+  findOne(filter: OptionalKeys<IUserToken>) {
+    const keys = Object.keys(filter);
+
+    return this.tokens.find(userToken => {
+      const results = keys.map(key => userToken[key] === filter[key]);
+
+      return !results.some(result => !result);
+    });
+  }
+  async findByToken(token: string) {
+    return this.findOne({ token });
   }
 }
