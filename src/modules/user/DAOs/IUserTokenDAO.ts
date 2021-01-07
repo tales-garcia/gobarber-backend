80fb2857ef82c7182923606590784d36e891ad24
Tales Garcia <garcia.tales@gmail.com>
2021-01-07 13:49:09 -0300

    feat(daos): create user token dao interface

---
+++ src/modules/user/DAOs/IUserTokenDAO.ts
@@ -0,0 +1,7 @@
+import IUserTokenDTO from "../DTOs/IUserTokenDTO";
+
+type IUserToken = Assign<IUserTokenDTO, "_id" | "token", string>;
+
+export default interface IUserTokenDAO {
+  generate(userId: string): Promise<IUserToken>;
+}
