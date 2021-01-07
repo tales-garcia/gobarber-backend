e984e1b517df237e12a6e33f3714fcfe1d99f364
Tales Garcia <garcia.tales@gmail.com>
2021-01-07 13:52:22 -0300

    feat(send forgot password email service): generate user token

--- src/modules/user/services/SendForgotPasswordEmailService.ts
+++ src/modules/user/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import IMailProvider from "@shared/container/providers/MailProvider/models/IMail
 import AppError from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
 import IUserDAO from "../DAOs/IUserDAO";
+import IUserTokenDAO from "../DAOs/IUserTokenDAO";
 
 @injectable()
 export default class SendForgotPasswordEmailService {
@@ -9,15 +10,19 @@ export default class SendForgotPasswordEmailService {
     @inject('UserDAO')
     private userDao: IUserDAO,
     @inject('MailProvider')
-    private mailProvider: IMailProvider
+    private mailProvider: IMailProvider,
+    @inject('UserTokenDAO')
+    private usersTokensDao: IUserTokenDAO
   ) { }
 
   public async execute({ email }: Assign<object, "email", string>) {
-    const user = await this.userDao.findByEmail(email);
+    const user = await this.userDao.findByEmail(email) as unknown as Assign<object, "_id", string>;
 
     if(!user) {
       throw new AppError('Error: User not found', 404);
     }
+
+    await this.usersTokensDao.generate(user._id);
 
     this.mailProvider.sendMail(email, 'Recuperação de senha');
   }
