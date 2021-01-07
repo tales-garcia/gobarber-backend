79fa9af335fff3bf8b0b475dcf14f22d6ff15f31
Tales Garcia <garcia.tales@gmail.com>
2021-01-07 15:36:36 -0300

    refactor(send forgot email service test): share lets around tests
    In before all callback, resets all lets

--- src/modules/user/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/user/services/SendForgotPasswordEmailService.spec.ts
@@ -5,12 +5,21 @@ import SendForgotPasswordEmailService from "./SendForgotPasswordEmailService";
 import AppError from '@shared/errors/AppError';
 import UserTokenDAOMock from '../DAOs/mocks/UserTokenDAOMock';
 
-describe('Send forgot password email', () => {
-  it('should be able to send a forgot password email', async () => {
-    const mailProvider = new MailProviderMock();
-    const userDao = new UserDAOMock();
-    const userTokenDao = new UserTokenDAOMock();
+let mailProvider: MailProviderMock;
+let userDao: UserDAOMock;
+let userTokenDao: UserTokenDAOMock;
+let service: SendForgotPasswordEmailService;
 
+describe('Send forgot password email', () => {
+  beforeEach(() => {
+    mailProvider = new MailProviderMock();
+    userDao = new UserDAOMock();
+    userTokenDao = new UserTokenDAOMock();
+
+    service = new SendForgotPasswordEmailService(userDao, mailProvider, userTokenDao);
+  });
+
+  it('should be able to send a forgot password email', async () => {
     const sendMail = jest.spyOn(mailProvider, 'sendMail');
 
     await userDao.create({
@@ -20,24 +29,15 @@ describe('Send forgot password email', () => {
       password: 'johndoe'
     });
 
-    await new SendForgotPasswordEmailService(userDao, mailProvider, userTokenDao).execute({ email: 'johndoe@example.com' });
+    await service.execute({ email: 'johndoe@example.com' });
 
     expect(sendMail).toBeCalledWith('johndoe@example.com', 'Recuperação de senha');
   });
   it('should not be able to send a forgot password email to an unregistered user', async () => {
-    const mailProvider = new MailProviderMock();
-    const userDao = new UserDAOMock();
-    const userTokenDao = new UserTokenDAOMock();
-
-    expect(new SendForgotPasswordEmailService(userDao, mailProvider, userTokenDao)
-      .execute({ email: 'johndoe@example.com' })
+    expect(service.execute({ email: 'johndoe@example.com' })
     ).rejects.toBeInstanceOf(AppError);
   });
   it('should be able to generate a user token', async () => {
-    const mailProvider = new MailProviderMock();
-    const userDao = new UserDAOMock();
-    const userTokenDao = new UserTokenDAOMock();
-
     const generate = jest.spyOn(userTokenDao, 'generate');
 
     const { _id: userId } = await userDao.create({
@@ -47,7 +47,7 @@ describe('Send forgot password email', () => {
       password: 'johndoe'
     });
 
-    await new SendForgotPasswordEmailService(userDao, mailProvider, userTokenDao).execute({ email: 'johndoe@example.com' });
+    await service.execute({ email: 'johndoe@example.com' });
 
     expect(generate).toBeCalledWith(userId);
   });
