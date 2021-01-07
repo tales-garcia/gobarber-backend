269c965c7e26ad8b3edd30b352f8dc08a2e4f96e
Tales Garcia <garcia.tales@gmail.com>
2021-01-07 18:17:55 -0300

    feat(iusertokendao): add findByToken method

--- src/modules/user/DAOs/IUserTokenDAO.ts
+++ src/modules/user/DAOs/IUserTokenDAO.ts
@@ -4,4 +4,5 @@ type IUserToken = Assign<IUserTokenDTO, "_id" | "token", string>;
 
 export default interface IUserTokenDAO {
   generate(userId: string): Promise<IUserToken>;
+  findByToken(token: string): Promise<IUserToken>;
 }
