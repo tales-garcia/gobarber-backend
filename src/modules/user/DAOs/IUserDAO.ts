14d2c8f31d3a736cb9e5e254fdf108c78c61aaa6
Tales Garcia <garcia.tales@gmail.com>
2021-01-07 17:17:25 -0300

    fix(iuserdao return type): change methods return type to return an user with id

--- src/modules/user/DAOs/IUserDAO.ts
+++ src/modules/user/DAOs/IUserDAO.ts
@@ -1,9 +1,11 @@
 import IUserDTO from "../DTOs/IUserDTO";
 
+type IUser = Assign<IUserDTO, "_id", string>;
+
 export default interface IUserDAO {
-  findByEmail(email: string): Promise<IUserDTO>;
-  findByIdAndUpdate(_id: string, query: OptionalKeys<IUserDTO>): Promise<IUserDTO>;
+  findByEmail(email: string): Promise<IUser>;
+  findByIdAndUpdate(_id: string, query: OptionalKeys<IUser>): Promise<IUser>;
   create(user: IUserDTO): Promise<Omit<IUserDTO, "password">>;
-  findById(_id: string): Promise<IUserDTO>;
-  find(filter?: OptionalKeys<Omit<IUserDTO, "password">>): Promise<Omit<IUserDTO, "password">[]>;
+  findById(_id: string): Promise<IUser>;
+  find(filter?: OptionalKeys<Omit<IUser, "password">>): Promise<Omit<IUser, "password">[]>;
 }
