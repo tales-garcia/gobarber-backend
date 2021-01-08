b5f61b20597aadfbd9544687012c02c1e3a486f4
Tales Garcia <garcia.tales@gmail.com>
2021-01-08 16:09:26 -0300

    test(reset password service): test if the password is hashed before saving the user

--- src/modules/user/services/ResetPasswordService.spec.ts
+++ src/modules/user/services/ResetPasswordService.spec.ts
@@ -1,21 +1,25 @@
 import 'reflect-metadata';
 import UserDAOMock from "../DAOs/mocks/UserDAOMock";
 import UserTokenDAOMock from '../DAOs/mocks/UserTokenDAOMock';
+import HashProviderMock from '../providers/HashProvider/mocks/HashProviderMock';
 import ResetPasswordService from './ResetPasswordService';
 
 let userDao: UserDAOMock;
 let userTokenDao: UserTokenDAOMock;
+let hashProvider: HashProviderMock;
 let service: ResetPasswordService;
 
 describe('Reset user password', () => {
   beforeEach(() => {
     userDao = new UserDAOMock();
     userTokenDao = new UserTokenDAOMock();
+    hashProvider = new HashProviderMock();
 
-    service = new ResetPasswordService(userDao, userTokenDao);
+    service = new ResetPasswordService(userDao, userTokenDao, hashProvider);
   });
 
   it('should be able to reset password', async () => {
+    const generateHash = jest.spyOn(hashProvider, 'generateHash');
 
     const { _id } = await userDao.create({
       avatar: undefined,
@@ -30,6 +34,7 @@ describe('Reset user password', () => {
 
     const user = await userDao.findById(_id);
 
+    expect(generateHash).toBeCalledWith('1223334444');
     expect(user).toHaveProperty('password', '1223334444');
 
   });
