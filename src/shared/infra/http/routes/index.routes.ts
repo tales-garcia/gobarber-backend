571a31c9ce55995b733badb97a559c0feb3beeb9
Tales Garcia <garcia.tales@gmail.com>
2021-01-09 19:25:38 -0300

    feat(routes): register password routes

--- src/shared/infra/http/routes/index.routes.ts
+++ src/shared/infra/http/routes/index.routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import appointments from '@modules/appointment/infra/http/appointments.routes';
 import users from '@modules/user/infra/http/users.routes';
 import sessions from '@modules/user/infra/http/sessions.routes';
+import password from '@modules/user/infra/http/password.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointments);
 routes.use('/users', users);
 routes.use('/sessions', sessions);
+routes.use('/password', password);
 
 export default routes;
