6a6ab13766a978f6aa70cd423ad8744859d476ec
Tales Garcia <garcia.tales@gmail.com>
2021-01-09 19:25:17 -0300

    feat(dependency injection): inject user token dao and, for now, mail provider mock

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -8,6 +8,12 @@ import AppointmentDAO from "@modules/appointment/infra/mongoose/DAOs/Appointment
 
 import IUserDAO from "@modules/user/DAOs/IUserDAO";
 import UserDAO from "@modules/user/infra/mongoose/DAOs/UserDAO";
+import IUserTokenDAO from "@modules/user/DAOs/IUserTokenDAO";
+import UserTokenDAO from "@modules/user/infra/mongoose/DAOs/UserTokenDAO";
+import IMailProvider from "./providers/MailProvider/models/IMailProvider";
+import MailProviderMock from "./providers/MailProvider/mocks/MailProviderMock";
 
 container.registerSingleton<IAppointmentDAO>('AppointmentDAO', AppointmentDAO);
 container.registerSingleton<IUserDAO>('UserDAO', UserDAO);
+container.registerSingleton<IUserTokenDAO>('UserTokenDAO', UserTokenDAO);
+container.registerSingleton<IMailProvider>('MailProvider', MailProviderMock);
