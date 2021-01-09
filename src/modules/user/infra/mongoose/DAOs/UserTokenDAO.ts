1e7431d434a009d959718b5337b295916afcc904
Tales Garcia <garcia.tales@gmail.com>
2021-01-09 19:23:51 -0300

    feat(daos): create mongoose user token DAO

---
+++ src/modules/user/infra/mongoose/DAOs/UserTokenDAO.ts
@@ -0,0 +1,18 @@
+import IUserTokenDAO from "@modules/user/DAOs/IUserTokenDAO";
+import IUserTokenDtO from "@modules/user/DTOs/IUserTokenDTO";
+import { uuid } from "uuidv4";
+import UserToken from "../entities/userToken"
+
+type TUserToken = Assign<Assign<IUserTokenDtO, "_id" | "token", string>, "createdAt" | "updatedAt", Date>;
+
+export default class UserTokenDAO implements IUserTokenDAO {
+  async generate(userId: string): Promise<TUserToken> {
+    return await UserToken.create({
+      userId,
+      token: uuid()
+    }) as unknown as TUserToken;
+  }
+  async findByToken(token: string): Promise<TUserToken> {
+    return await UserToken.findOne({ token }) as unknown as TUserToken;
+  }
+}
