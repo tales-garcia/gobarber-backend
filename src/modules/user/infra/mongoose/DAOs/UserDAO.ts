2a2b9fa67de818c545d159cb0f3275732ec8b6ab
Tales Garcia <garcia.tales@gmail.com>
2021-01-09 18:58:41 -0300

    fix(user dao): fix methods return types

--- src/modules/user/infra/mongoose/DAOs/UserDAO.ts
+++ src/modules/user/infra/mongoose/DAOs/UserDAO.ts
@@ -6,20 +6,20 @@ interface IUser extends Assign<IUserDtO, "_id", string> {}
 
 export default class UserDAO implements IUserDAO {
   async findByEmail(email: string) {
-    return await User.findOne({ email }) as unknown as IUserDtO;
+    return await User.findOne({ email }) as unknown as IUser;
   }
   async findById(_id: string) {
-    return await User.findById(_id) as unknown as IUserDtO;
+    return await User.findById(_id) as unknown as IUser;
   }
   async findByIdAndUpdate(_id: string, query: OptionalKeys<IUser>) {
     return await User.findByIdAndUpdate(_id, {
       $set: {
         ...query
       }
-    }) as unknown as IUserDtO;
+    }) as unknown as IUser;
   }
   async find(filter?: OptionalKeys<IUser>) {
-    const users = await User.find(filter) as unknown as IUserDtO[];
+    const users = await User.find(filter) as unknown as IUser[];
 
     users.forEach(user => user.password = undefined);
 
