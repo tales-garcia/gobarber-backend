cb93334c1bfa3ca97f8a9a069f23eeb6f3859f0c
Tales Garcia <garcia.tales@gmail.com>
2021-01-10 19:16:33 -0300

    feat(mail provider): add subject param

--- src/shared/container/providers/MailProvider/mocks/MailProviderMock.ts
+++ src/shared/container/providers/MailProvider/mocks/MailProviderMock.ts
@@ -3,12 +3,13 @@ import IMailProvider from "../models/IMailProvider";
 interface Message {
   to: string;
   body: string;
+  subject: string;
 }
 
 export default class MailProviderMock implements IMailProvider {
   private messages: Message[] = [];
 
-  async sendMail(to: string, body: string): Promise<void> {
-    this.messages.push({ to, body });
+  async sendMail(to: string, subject: string, body: string): Promise<void> {
+    this.messages.push({ to, subject, body });
   }
 }
