38152f127886f85add446d1b7937305b866a051d
Tales Garcia <garcia.tales@gmail.com>
2021-01-10 19:32:34 -0300

    refactor(dependency injection): move mail provider injection to providers folder

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,5 +1,8 @@
 import { container } from "tsyringe";
 import DiskStorageProvider from "./StorageProvider/implementations/DiskStorageProvider";
 import IStorageProvider from "./StorageProvider/models/IStorageProvider";
+import IMailProvider from "./MailProvider/models/IMailProvider";
+import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
 
+container.registerSingleton<IMailProvider>('MailProvider', EtherealMailProvider);
 container.registerSingleton<IStorageProvider>('StorageProvider', DiskStorageProvider);
