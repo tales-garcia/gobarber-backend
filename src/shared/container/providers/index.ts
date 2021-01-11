e00890ccb7cd470b5946eac29db417d3660bc97e
Tales Garcia <garcia.tales@gmail.com>
2021-01-11 17:48:10 -0300

    feat(dependency injection): inject handle bars mail template provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,9 @@ import DiskStorageProvider from "./StorageProvider/implementations/DiskStoragePr
 import IStorageProvider from "./StorageProvider/models/IStorageProvider";
 import IMailProvider from "./MailProvider/models/IMailProvider";
 import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
+import HandleBarsMailTemplateProvider from "./MailTemplateProvider/implementations/HandleBarsMailTemplateProvider";
+import IMailTemplateProvider from "./MailTemplateProvider/models/IMailTemplateProvider";
 
 container.registerInstance<IMailProvider>('MailProvider', new EtherealMailProvider());
 container.registerSingleton<IStorageProvider>('StorageProvider', DiskStorageProvider);
+container.registerSingleton<IMailTemplateProvider>('MailTemplateProvider', HandleBarsMailTemplateProvider);
