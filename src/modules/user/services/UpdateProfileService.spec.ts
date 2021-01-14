b5b1850a53b001f5c67728441ed9c1fc09dda2ab
Tales Garcia <garcia.tales@gmail.com>
2021-01-14 20:25:57 -0300

    test(update profile service): create new tests
    Test if the service throws an error if the email is already used, if the old password is not
    provided, and if the old password does not match

--- src/modules/user/services/UpdateProfileService.spec.ts
+++ src/modules/user/services/UpdateProfileService.spec.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import HashProviderMock from "../providers/HashProvider/mocks/HashProviderMock";
 import UserDAOMock from "../DAOs/mocks/UserDAOMock";
 import UpdateProfileService from "./UpdateProfileService";
+import AppError from '@shared/errors/AppError';
 
 let service: UpdateProfileService;
 let userDao: UserDAOMock;
@@ -42,5 +43,61 @@ describe('Update user profile', () => {
     expect(updatedUser).toHaveProperty('password', '123456');
     expect(updatedUser).toHaveProperty('email', 'john.doe@example.com');
     expect(updatedUser).toHaveProperty('name', 'Johndoe');
+  });
+  it('should not be able to update the user password without old password', async () => {
+
+    const { _id: userId } = await userDao.create({
+      avatar: undefined,
+      email: 'johndoe@example.com',
+      name: 'John Doe',
+      password: 'johndoe'
+    });
+
+    await expect(service.execute({
+      updateQuery: {
+        password: '123456',
+        name: 'Johndoe',
+        email: 'john.doe@example.com'
+      },
+      userId
+    })).rejects.toBeInstanceOf(AppError);
+  });
+  it('should not be able to update the user password with a wrong old password', async () => {
+
+    const { _id: userId } = await userDao.create({
+      avatar: undefined,
+      email: 'johndoe@example.com',
+      name: 'John Doe',
+      password: 'johndoe'
+    });
+
+    await expect(service.execute({
+      oldPassword: '1234567',
+      updateQuery: {
+        password: '123456',
+        name: 'Johndoe',
+        email: 'john.doe@example.com'
+      },
+      userId
+    })).rejects.toBeInstanceOf(AppError);
+  });
+  it('should not be able to update the user email if the email is already used', async () => {
+
+    const { _id: userId } = await userDao.create({
+      avatar: undefined,
+      email: 'johndoe@example.com',
+      name: 'John Doe',
+      password: 'johndoe'
+    });
+
+    await expect(service.execute({
+      oldPassword: '1234567',
+      updateQuery: {
+        password: '123456',
+        name: 'Johndoe',
+        email: 'john.doe@example.com'
+      },
+      userId
+    })).rejects.toBeInstanceOf(AppError);
   });
 });
