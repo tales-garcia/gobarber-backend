234de5f80fbbe0f61a5fd3d25beab309f51ca61b
Tales Garcia <garcia.tales@gmail.com>
2021-01-14 20:23:22 -0300

    feat(update profile service): make old password required
    Only change the password if the old password is provided and if it matches

--- src/modules/user/services/UpdateProfileService.ts
+++ src/modules/user/services/UpdateProfileService.ts
@@ -6,6 +6,7 @@ import IHashProvider from "../providers/HashProvider/models/IHashProvider";
 
 interface Request {
   userId: string;
+  oldPassword?: string;
   updateQuery: OptionalKeys<IUserDtO>;
 }
 
@@ -18,19 +19,32 @@ export default class UpdateProfileService {
     private hashProvider: IHashProvider
   ) { }
 
-  public async execute({ userId, updateQuery }: Request) {
+  public async execute({ userId, updateQuery, oldPassword }: Request) {
     const user = await this.userDao.findById(userId);
+
+    if (updateQuery.email) {
+      if (await this.userDao.findByEmail(updateQuery.email)) {
+        throw new AppError('Error: Email already used', 400);
+      }
+    }
 
     if (!user) {
       throw new AppError('Error: User not found', 404);
     }
 
-    if (updateQuery.password) {
+    if (updateQuery.password && !oldPassword) {
+      throw new AppError('Error: Old password required', 403);
+    }
+
+    if (updateQuery.password && oldPassword) {
+      if (!await this.hashProvider.compareHash(oldPassword, updateQuery.password)) {
+        throw new AppError('Error: Wrong password', 401);
+      }
       Object.assign(updateQuery, {
         password: await this.hashProvider.generateHash(updateQuery.password)
       });
     }
 
-    await this.userDao.findByIdAndUpdate(userId, updateQuery);
+    return await this.userDao.findByIdAndUpdate(userId, updateQuery);
   }
 }
