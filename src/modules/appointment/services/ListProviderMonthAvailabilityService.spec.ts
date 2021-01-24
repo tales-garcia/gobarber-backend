574aacb1c99bdfdd5ff06b3ae7b9136893c65ea4
Tales Garcia <garcia.tales@gmail.com>
2021-01-24 09:18:27 -0300

    test(list provider month availability): create multiple appointments before test

--- src/modules/appointment/services/ListProviderMonthAvailabilityService.spec.ts
+++ src/modules/appointment/services/ListProviderMonthAvailabilityService.spec.ts
@@ -34,6 +34,51 @@ describe('List the month availability', () => {
       date: new Date(2021, 4, 21, 8, 0, 0)
     });
 
+    await appointmentDao.create({
+      providerId: 'user',
+      date: new Date(2021, 4, 21, 9, 0, 0)
+    });
+
+    await appointmentDao.create({
+      providerId: 'user',
+      date: new Date(2021, 4, 21, 10, 0, 0)
+    });
+
+    await appointmentDao.create({
+      providerId: 'user',
+      date: new Date(2021, 4, 21, 11, 0, 0)
+    });
+
+    await appointmentDao.create({
+      providerId: 'user',
+      date: new Date(2021, 4, 21, 12, 0, 0)
+    });
+
+    await appointmentDao.create({
+      providerId: 'user',
+      date: new Date(2021, 4, 21, 13, 0, 0)
+    });
+
+    await appointmentDao.create({
+      providerId: 'user',
+      date: new Date(2021, 4, 21, 14, 0, 0)
+    });
+
+    await appointmentDao.create({
+      providerId: 'user',
+      date: new Date(2021, 4, 21, 15, 0, 0)
+    });
+
+    await appointmentDao.create({
+      providerId: 'user',
+      date: new Date(2021, 4, 21, 16, 0, 0)
+    });
+
+    await appointmentDao.create({
+      providerId: 'user',
+      date: new Date(2021, 4, 21, 17, 0, 0)
+    });
+
     const availability = await service.execute({
       providerId: 'user',
       year: 2021,
@@ -43,7 +88,7 @@ describe('List the month availability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { day: 19, available: true },
-        { day: 20, available: false },
+        { day: 20, available: true },
         { day: 21, available: false },
         { day: 22, available: true }
       ])
