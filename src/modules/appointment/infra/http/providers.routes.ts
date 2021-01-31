24e0287dc753e9eb64ef1de6e8e9bb0d009e8b5c
Tales Garcia <garcia.tales@gmail.com>
2021-01-31 13:02:30 -0300

    feat(routes): register month and day availability controllers

--- src/modules/appointment/infra/http/providers.routes.ts
+++ src/modules/appointment/infra/http/providers.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import ProvidersController from './controllers/ProvidersController';
+import DayAvailabilityController from './controllers/DayAvailabilityController';
+import MonthAvailabilityController from './controllers/MonthAvailabilityController';
 import authentication from '@modules/user/infra/http/middlewares/authentication';
 
 const routes = Router();
@@ -7,5 +9,7 @@ const routes = Router();
 routes.use(authentication);
 
 routes.get('/', ProvidersController.index);
+routes.get('/:id/availability/month', MonthAvailabilityController.index);
+routes.get('/:id/availability/day', DayAvailabilityController.index);
 
 export default routes;
