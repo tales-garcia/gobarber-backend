7b4f7dbf6319c60fc3cb741ffd22b90590901fa8
Tales Garcia <garcia.tales@gmail.com>
2021-01-31 09:40:48 -0300

    test(create appointment): create new tests
    Test if the service throws an AppError when: the provided ids are equal, the provided date is past,
    the provided date dont match the schedule

--- src/modules/appointment/services/CreateAppointmentService.spec.ts
+++ src/modules/appointment/services/CreateAppointmentService.spec.ts
@@ -45,4 +45,36 @@ describe('Create appointment', () => {
     )).rejects.toBeInstanceOf(AppError);
 
   });
+  it('should not be able to create an appointment on a past date', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => new Date(2021, 4, 21, 11).getTime());
+
+    await expect(service.execute({
+      clientId: 'client',
+      date: new Date(2021, 4, 21, 10).toISOString(),
+      providerId: 'provider'
+    })).rejects.toBeInstanceOf(AppError);
+  });
+  it('should not be able to create an appointment with same user as provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => new Date(2021, 4, 21, 11).getTime());
+
+    await expect(service.execute({
+      clientId: 'id',
+      date: new Date(2021, 4, 21, 13).toISOString(),
+      providerId: 'id'
+    })).rejects.toBeInstanceOf(AppError);
+  });
+  it('should not be able to create an appointment before 8am or after 5pm', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => new Date(2020, 4, 10, 12).getTime());
+
+    await expect(service.execute({
+      clientId: 'client-id',
+      date: new Date(2020, 4, 11, 7).toISOString(),
+      providerId: 'provider-id'
+    })).rejects.toBeInstanceOf(AppError);
+    await expect(service.execute({
+      clientId: 'client-id',
+      date: new Date(2020, 4, 11, 18).toISOString(),
+      providerId: 'provider-id'
+    })).rejects.toBeInstanceOf(AppError);
+  });
 });
