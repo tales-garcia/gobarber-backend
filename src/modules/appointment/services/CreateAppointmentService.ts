a434f80aae996689d7cc9d281a0c535941cd5b3b
Tales Garcia <garcia.tales@gmail.com>
2021-01-31 10:46:36 -0300

    feat(create appointment service): implement errors

--- src/modules/appointment/services/CreateAppointmentService.ts
+++ src/modules/appointment/services/CreateAppointmentService.ts
@@ -1,6 +1,7 @@
 import IAppointmentDAO from "@modules/appointment/DAOs/IAppointmentDAO";
 import AppError from "@shared/errors/AppError";
-import { parseISO, startOfHour } from "date-fns";
+import { isBefore, parseISO, startOfHour } from "date-fns";
+import { getHours } from "date-fns/esm";
 import { inject, injectable } from "tsyringe";
 
 interface Request {
@@ -20,6 +21,18 @@ export default class CreateAppointmentService {
     const parsedDate = startOfHour(parseISO(date));
 
     const appointmentInSameDate = await this.appointmentDao.findByDate(parsedDate);
+
+    if (isBefore(parsedDate, Date.now())) {
+      throw new AppError('Error: Invalid Date', 405);
+    }
+
+    if (providerId === clientId) {
+      throw new AppError('Error: Invalid ProviderId', 403);
+    }
+
+    if (getHours(parsedDate) < 8 || getHours(parsedDate) > 17) {
+      throw new AppError('Error: Invalid hour', 406);
+    }
 
     if (appointmentInSameDate) {
       throw new AppError('Error: Appointment already booked', 400);
