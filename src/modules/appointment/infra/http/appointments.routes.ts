11a3db575aa99d89a555da8897e400ee618e36b3
Tales Garcia <garcia.tales@gmail.com>
2021-01-31 16:15:10 -0300

    feat(routes): register provider appointments controller on appointments route file

--- src/modules/appointment/infra/http/appointments.routes.ts
+++ src/modules/appointment/infra/http/appointments.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import AppointmentController from './controllers/AppointmentController';
+import ProviderAppointmentsController from './controllers/ProviderAppointmentsController';
 import authentication from '@modules/user/infra/http/middlewares/authentication';
 
 const routes = Router();
@@ -8,5 +9,6 @@ routes.use(authentication);
 
 routes.post('/', AppointmentController.create);
 routes.get('/', AppointmentController.index);
+routes.get('/me', ProviderAppointmentsController.index);
 
 export default routes;
