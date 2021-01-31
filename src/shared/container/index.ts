6715c6c717401a8c53cfc099830af545295f0389
Tales Garcia <garcia.tales@gmail.com>
2021-01-31 20:02:00 -0300

    feat(dependency injection): inject notifications dao

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,7 +10,10 @@ import IUserDAO from "@modules/user/DAOs/IUserDAO";
 import UserDAO from "@modules/user/infra/mongoose/DAOs/UserDAO";
 import IUserTokenDAO from "@modules/user/DAOs/IUserTokenDAO";
 import UserTokenDAO from "@modules/user/infra/mongoose/DAOs/UserTokenDAO";
+import NotificationDAO from "@modules/notifications/infra/mongoose/DAOs/NotificationDAO";
+import INotificationDAO from "@modules/notifications/DAOs/INotificationDAO";
 
 container.registerSingleton<IAppointmentDAO>('AppointmentDAO', AppointmentDAO);
 container.registerSingleton<IUserDAO>('UserDAO', UserDAO);
 container.registerSingleton<IUserTokenDAO>('UserTokenDAO', UserTokenDAO);
+container.registerSingleton<INotificationDAO>('NotificationsDAO', NotificationDAO);
