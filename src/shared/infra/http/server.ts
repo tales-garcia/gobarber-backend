435ed889ae1ad17a77cc5cd8ac3aba81db9f0dcd
Tales Garcia <garcia.tales@gmail.com>
2021-02-01 15:35:03 -0300

    feat(server.ts): use global error handler

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 import '@shared/container';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import routes from './routes/index.routes';
 import cors from 'cors';
 import { uploadsFolder } from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -11,5 +13,14 @@ app.use(cors());
 app.use(express.json());
 app.use('/images', express.static(uploadsFolder));
 app.use(routes);
+
+app.use((e: Error, req: Request, res: Response, _: NextFunction) => {
+  if (e instanceof AppError) {
+    return res.status(e.statusCode).json({ msg: e.message });
+  }
+
+  console.log(e);
+  return res.status(500).json({ msg: 'Error: Internal server error', err: e });
+});
 
 app.listen(3333);
