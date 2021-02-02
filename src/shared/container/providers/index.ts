b578d37b0fb26af24306e0bcec0c226396f7e036
Tales Garcia <garcia.tales@gmail.com>
2021-02-02 18:28:29 -0300

    fix(container providers index): resolve ses mail provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,7 +5,13 @@ import IMailProvider from "./MailProvider/models/IMailProvider";
 import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
 import HandleBarsMailTemplateProvider from "./MailTemplateProvider/implementations/HandleBarsMailTemplateProvider";
 import IMailTemplateProvider from "./MailTemplateProvider/models/IMailTemplateProvider";
+import mailConfig from '@config/mail';
+import SESMailProvider from "./MailProvider/implementations/SESMailProvider";
 
 container.registerSingleton<IStorageProvider>('StorageProvider', DiskStorageProvider);
 container.registerSingleton<IMailTemplateProvider>('MailTemplateProvider', HandleBarsMailTemplateProvider);
-container.registerInstance<IMailProvider>('MailProvider', container.resolve(EtherealMailProvider));
+container.registerInstance<IMailProvider>('MailProvider', mailConfig.driver === 'ethereal' ?
+  container.resolve(EtherealMailProvider)
+  :
+  container.resolve(SESMailProvider)
+);
