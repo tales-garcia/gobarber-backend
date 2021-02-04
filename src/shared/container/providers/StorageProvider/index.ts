fe120bb57c82af0ecee3f73135eb4bab08b2cae4
Tales Garcia <garcia.tales@gmail.com>
2021-02-03 22:19:39 -0300

    refactor(storage provider index): use s3 storage implementation

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -1,5 +1,12 @@
 import { container } from "tsyringe";
 import DiskStorageProvider from "./implementations/DiskStorageProvider";
+import S3StorageProvider from "./implementations/S3StorageProvider";
 import IStorageProvider from "./models/IStorageProvider";
+import uploadConfig from '@config/upload';
 
-container.registerSingleton<IStorageProvider>('StorageProvider', DiskStorageProvider);
+const providers = {
+  disk: DiskStorageProvider,
+  aws: S3StorageProvider
+}
+
+container.registerSingleton<IStorageProvider>('StorageProvider', providers[uploadConfig.driver]);
