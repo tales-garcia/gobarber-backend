2df78f828ce65bc83ea40c48627544eba52e0364
Tales Garcia <garcia.tales@gmail.com>
2021-02-04 22:19:27 -0300

    refactor(icacheprovider): return any instead of string

--- src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
+++ src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -1,5 +1,5 @@
 export default interface ICacheProvider {
-  save(key: string, value: string): Promise<void>;
-  recover(key: string): Promise<string>;
+  save(key: string, value: any): Promise<void>;
+  recover(key: string): Promise<any>;
   invalidate(key: string): Promise<void>;
 }
