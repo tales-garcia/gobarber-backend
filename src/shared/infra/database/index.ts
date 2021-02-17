a26780155e695168c36fdf6cb1c3b25bdf63ac79
Tales Garcia <garcia.tales@gmail.com>
2021-02-17 10:02:32 -0300

    feat(database): use authentication

--- src/shared/infra/database/index.ts
+++ src/shared/infra/database/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/gobarber`, {
+mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/gobarber?authMechanism=SCRAM-SHA-1`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
